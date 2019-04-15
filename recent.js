504503307342c510a52f334ebfb81e681cf677bc
hippomu <44453660+hippomu@users.noreply.github.com>
2019-04-15 20:12:26 +0700

    Update recent.js

--- recent.js
+++ recent.js
@@ -12,8 +12,4 @@ if(n.link[o].rel=="alternate"){i=n.link[o].href;break}}
 var l;try{l=n.media$thumbnail.url}catch(h){s=n.content.$t;a=s.indexOf("<img");b=s.indexOf('src="',a);c=s.indexOf('"',b+5);d=s.substr(b+5,c-b-5);if(a!=-1&&b!=-1&&c!=-1&&d!=""){l=d}else l="https://2.bp.blogspot.com/-giova1ZCh-A/Uzq6L8QTJNI/AAAAAAAAAJc/USXictTq_xs/s70-c/KM+Icon.png"}
 var p=n.published.$t;var v=p.substring(0,4);var m=p.substring(5,7);var g=p.substring(8,10);var y=new Array;document.write('<li>');document.write('<strong><a href="'+i+'" target ="_blank" title="'+r+'" rel="nofollow"><img class="label_thumb_40" alt="'+r+'" src="'+l+'"/><div class="download-title">'+r+'</div></a></strong>');document.write('');var x="";var T=0;if(t!=numposts-1)document.write("")}
 document.write("</ul>")}
-
-//Script Image Resize
-$(document).ready(function(){$('.label_thumb').attr('src',function(i,src){return src.replace('w300-h300-p-k-no-nu','w300-h300-p-k-no-nu')})})
-$(document).ready(function(){$('.label_thumb_40').attr('src',function(i,src){return src.replace('s40-c','s40-c')})})
 //]]>
